8b15ae06a41106ddd24f2d67f0ae61cd7ba17ef6
JinYeob Kim <jinyeob07@gmail.com>
2020-02-22 21:54:28 +0900

    first init

---
+++ app/src/main/java/com/jinyeob/mynews/MyAdapter.java
@@ -0,0 +1,66 @@
+package com.jinyeob.mynews;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    //한 줄에 들어가는 요소
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextViewTitle;
+        public TextView mTextViewContent;
+        public ImageView mImageView;
+
+        public MyViewHolder(View v) {
+            super(v);
+            mTextViewTitle = v.findViewById(R.id.TextView_title);
+            mTextViewContent = v.findViewById(R.id.TextView_content);
+            mImageView = v.findViewById(R.id.ImageView_title);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    //전체화면
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // create a new view
+        LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_news, parent, false);
+        //text 리스트 하나하나 xml필요
+
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextViewTitle.setText(mDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
